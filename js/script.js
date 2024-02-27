a62dcb070f4f26d5adebe44ddbc70b0eaa09ad0e
RekkaESP <carloscastilloandrades@gmail.com>
2024-02-27 13:04:42 +0100

    Add custom size grid creation

--- js/script.js
+++ js/script.js
@@ -1,16 +1,33 @@
-makeGrid(5);
+
 const cards = document.querySelectorAll('.card');
+const sizeButton = document.getElementById('button-size');
+let color = 'red';
+let currentSize = 0;
+
+sizeButton.addEventListener('click', (e) => {
+    let size = parseInt(prompt('Choose grid size in squares per line',4),10);
+    clearGrid();
+    if(size > 100){
+        alert("Size selected over limit (100x100 max)")
+        size = 4;
+    }
+    if(size == NaN){
+        size = 4;
+    }
+    makeGrid(size);
+});
 
 cards.forEach((card) => {
     card.addEventListener('mouseover', (e) => {
-        card.style.backgroundColor = 'red';
+        card.style.backgroundColor = color;
     });
 });
+
 function makeGrid(size){
     let container = document.getElementById('container-grid');
-
+    currentSize = size;
     //Calculate card size at creation
-    const gridSize = parseInt(getComputedStyle(container).width);
+    const gridSize = parseInt(getComputedStyle(container).flexBasis);
     let cardSize = (gridSize/size)+"px";
 
     for(var i = 0; i < size; i++){
@@ -20,8 +37,20 @@ function makeGrid(size){
             let aux = document.createElement('div');
             aux.classList.add('card');
             aux.style.flexBasis = cardSize;
+
+            aux.addEventListener('mouseover', (e) => {
+                aux.style.backgroundColor = color;
+            });
             line.appendChild(aux);
         }
         container.appendChild(line);
     }
+}
+
+function clearGrid(){
+    let container = document.getElementById('container-grid');
+    let lines = document.querySelectorAll('.line');
+    lines.forEach((line) => {
+        container.removeChild(line);
+    });
 }
